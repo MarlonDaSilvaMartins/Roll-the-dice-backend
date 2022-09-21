d159dce6167f36237541db24e727a9d0b62770e5
Marlon <marlon673@hotmail.com>
2022-09-21 11:39:06 -0300

    Commit inicial do projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+# Roll-the-dice-backend
+
+###Projeto de backend para a cadeira de Engenharia de Software B na Faculdade Dom Bosco
+
+###Objetivo: Criar API para gerenciar fichas de RPG
---
+++ src/test/java/com/rollthedice/RollTheDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rollthedice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollTheDiceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
